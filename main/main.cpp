21d424b5ffa5257a1f381fb51488585bf1bb0b66
twistedkateKing <esrobb3@gmail.com>
2019-11-26 22:35:24 +0900

    [#3] edge detection main 연결

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
+#include "src/edge_drawing.h"
 
 int main(void)
 {
-  
+  EdgeDrawing edge_detector();
+
   cv::Mat rena_img = cv::imread("main/rena.png");
   cv::imshow("rena", rena_img);
   cv::waitKey(0);
