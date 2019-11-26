1a830888c8a2720d63442409cca7805eacbf6517
twistedkateKing <esrobb3@gmail.com>
2019-11-26 22:30:49 +0900

    [#3] ED cpp 생성.

---
+++ src/edge_drawing.cpp
@@ -0,0 +1,16 @@
+//
+// Created by cjyj on 19. 11. 26..
+//
+
+#include "edge_drawing.h"
+
+EdgeDrawing::EdgeDrawing() {
+
+}
+
+EdgeDrawing::~EdgeDrawing() {
+
+}
+void EdgeDrawing::detect() {
+
+}
---
+++ src/edge_drawing.h
@@ -0,0 +1,17 @@
+//
+// Created by cjyj on 19. 11. 26..
+//
+
+#ifndef EDCIRCLE_SRC_EDGE_DRAWING_H_
+#define EDCIRCLE_SRC_EDGE_DRAWING_H_
+
+class EdgeDrawing {
+ public:
+  EdgeDrawing();
+  ~EdgeDrawing();
+
+  void detect();
+
+};
+
+#endif //EDCIRCLE_SRC_EDGE_DRAWING_H_
