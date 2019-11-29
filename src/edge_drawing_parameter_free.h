52df59e888fd7636b69db2c650116d3449e6ab20
twistedkateKing <esrobb3@gmail.com>
2019-11-29 19:53:12 +0900

    [#2] EDPF class 생성.

---
+++ src/edge_drawing_parameter_free.h
@@ -0,0 +1,12 @@
+//
+// Created by cjyj on 19. 11. 29..
+//
+
+#ifndef EDCIRCLE_SRC_EDGE_DRAWING_PARAMETER_FREE_H_
+#define EDCIRCLE_SRC_EDGE_DRAWING_PARAMETER_FREE_H_
+
+class edge_drawing_parameter_free {
+
+};
+
+#endif //EDCIRCLE_SRC_EDGE_DRAWING_PARAMETER_FREE_H_
