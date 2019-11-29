eeb35013d6e3c26f2cadc82a67bb8a0bcac8cefc
twistedkateKing <esrobb3@gmail.com>
2019-11-30 01:23:49 +0900

    [#2] main drawing code 수정.

--- main/main.cpp
+++ main/main.cpp
@@ -9,12 +9,25 @@ int main(void)
   cv::Mat rena_img = cv::imread("main/Lenna.png");
   cv::imshow("renna", rena_img);
 
-  EdgeDrawing ed;
-  std::vector< std::vector<cv::Point> > ret1 = ed.detect(rena_img);
-
+//  EdgeDrawing ed;
+//  std::vector< std::vector<cv::Point> > edge_segment = ed.detect(rena_img);
   EdgeDrawingParameterFree edpf;
-  std::vector< std::vector<cv::Point> > ret2 = edpf.detect(rena_img);
+  std::vector< std::vector<cv::Point> > edge_segment = edpf.detect(rena_img);
 
+  cv::Mat ret_img_segment = cv::Mat::zeros(rena_img.size(), CV_8UC3);
+  cv::Mat ret_img_edge = cv::Mat::zeros(rena_img.size(), CV_8UC3);
+
+  for(int i =0 ; i < edge_segment.size(); i++) {
+    cv::Vec3b ran_color = cv::Vec3b(rand()%255,rand()%255,rand()%255);
+    for (int j = 0; j < edge_segment[i].size(); j++) {
+      ret_img_segment.at<cv::Vec3b>(edge_segment[i][j]) = ran_color;
+      ret_img_edge.at<cv::Vec3b>(edge_segment[i][j]) = cv::Vec3b(255,255,255);
+    }
+  }
+
+  cv::imshow("ret_img_segment", ret_img_segment);
+  cv::imshow("ret_img_edge", ret_img_edge);
   cv::waitKey(0);
+
   return 1;
 }
