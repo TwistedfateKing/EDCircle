f8a7b188178908b026e8256ac4b682ed9229f57b
twistedkateKing <esrobb3@gmail.com>
2019-12-01 06:30:57 +0900

    [#1] least square error ellipse fitting 함수... 작성...

--- src/ed_circle.cpp
+++ src/ed_circle.cpp
@@ -41,7 +41,7 @@ std::vector<std::vector<cv::Point>> EDCircle::find_least_squares_circle_fit(std:
     float radius;
     bool check = least_squares_circle_fit(edge_segment[i], circle_center, radius);
     if( check == true) {
-
+      std::cout << " Find Circle " << std::endl;
     }
     else {
       edge_segment_l.push_back(edge_segment[i]);
@@ -52,24 +52,29 @@ std::vector<std::vector<cv::Point>> EDCircle::find_least_squares_circle_fit(std:
 }
 
 std::vector<std::vector<cv::Point>> EDCircle::find_least_squares_ellipse_fit(std::vector<std::vector<cv::Point> > edge_segment) {
-  std::vector<std::vector<cv::Point> > edge_segment_l = edge_segment;
+  std::vector<std::vector<cv::Point> > edge_segment_l;
+
+  for(int i = 0; i < edge_segment.size(); i++)
+  {
+    float a,b,c,d,e,f;
+    a = b = c = d = e = f = 0;
+
+    bool check = least_squares_ellipse_fit(edge_segment[i], a, b, c, d, e, f);
+    if( check == true) {
+      std::cout << " Find Ellipse " << std::endl;
+    }
+    else {
+      edge_segment_l.push_back(edge_segment[i]);
+    }
+  }
+
   return edge_segment_l;
 }
 
 bool EDCircle::least_squares_circle_fit(std::vector<cv::Point> edge_segment,
                                         cv::Point2f &circle_center,
                                         float &radius) {
-
-  std::vector<cv::Point2f> test;
-  test.push_back(cv::Point2f(0, 0));
-  test.push_back(cv::Point2f(0.5, 0.25));
-  test.push_back(cv::Point2f(1, 1));
-  test.push_back(cv::Point2f(1.5, 2.25));
-  test.push_back(cv::Point2f(2, 4));
-  test.push_back(cv::Point2f(2.5, 6.25));
-  test.push_back(cv::Point2f(3, 9));
-
-  int N = test.size();
+  int N = edge_segment.size();
   float x_mean = 0;
   float y_mean = 0;
   float Suu = 0;
@@ -81,15 +86,15 @@ bool EDCircle::least_squares_circle_fit(std::vector<cv::Point> edge_segment,
   float Svuu = 0;
 
   for(int i = 0; i < N; i++) {
-    x_mean += test[i].x;
-    y_mean += test[i].y;
+    x_mean += edge_segment[i].x;
+    y_mean += edge_segment[i].y;
   }
   x_mean = x_mean / (float)N;
   y_mean = y_mean / (float)N;
 
   for(int i = 0; i < N; i++) {
-    float u = test[i].x - x_mean;
-    float v = test[i].y - y_mean;
+    float u = edge_segment[i].x - x_mean;
+    float v = edge_segment[i].y - y_mean;
     Suu += u*u;
     Suv += u*v;
     Svv += v*v;
@@ -111,14 +116,140 @@ bool EDCircle::least_squares_circle_fit(std::vector<cv::Point> edge_segment,
   B.at<float>(1) = (Svvv + Svuu)/2.0f;
   cv::Mat uv_center = A.inv() * B;
 
-  std::cout << A << std::endl;
-  std::cout << B << std::endl;
-  std::cout << uv_center << std::endl;
+//  std::cout << A << std::endl;
+//  std::cout << B << std::endl;
+//  std::cout << uv_center << std::endl;
   float uc = uv_center.at<float>(0);
   float vc = uv_center.at<float>(1);
   float x_center = uc + x_mean;
   float y_center = vc + y_mean;
   float R = sqrt(uc*uc + vc*vc + (Suu+Svv)/N);
 
-  return false;
+  // error check
+  float error = 0;
+  bool ret = true;
+  for( int i = 0 ; i < N ; i++){
+    float x = edge_segment[i].x;
+    float y = edge_segment[i].y;
+    error = sqrt(fabs((x - x_center)*(x - x_center) + (y - y_center)*(y - y_center) - R*R));
+    if( error > 1.5) {
+      ret = false;
+      break;
+    }
+  }
+
+  return ret;
+}
+bool EDCircle::least_squares_ellipse_fit(std::vector<cv::Point> edge_segment,
+                                         float &a,
+                                         float &b,
+                                         float &c,
+                                         float &d,
+                                         float &e,
+                                         float &f) {
+
+  int N = edge_segment.size();
+  float mx, my, sx, sy;
+  mx = my = sx = sy = 0;
+  cv::Mat D1 = cv::Mat::zeros(cv::Size(3, N),CV_32FC1);
+  cv::Mat D2 = cv::Mat::zeros(cv::Size(3, N),CV_32FC1);
+
+  float xmin,xmax,ymin,ymax;
+  xmin = edge_segment[0].x;
+  xmax = edge_segment[0].x;
+  ymin = edge_segment[0].y;
+  ymax = edge_segment[0].y;
+  for(int i = 0; i < N; i++) {
+    float x = edge_segment[i].x;
+    float y = edge_segment[i].y;
+    mx += x;
+    my += y;
+    // min max
+    if(xmax < x)
+      xmax = x;
+    if(xmin > x)
+      xmin = x;
+    if(ymax < y)
+      ymax = y;
+    if(ymin > y)
+      ymin = y;
+  }
+  mx = mx / (float)N;
+  my = my / (float)N;
+  std::cout << mx << std::endl;
+  std::cout << my << std::endl;
+
+  // Make D matrix
+  for ( int i = 0; i < N; i++) {
+    float x1 = (edge_segment[i].x-mx);
+    float y1 = (edge_segment[i].y-my);
+    D1.at<float>(i, 0) = (float)x1*x1;
+    D1.at<float>(i, 1) = (float)x1*y1;
+    D1.at<float>(i, 2) = (float)y1*y1;
+    D2.at<float>(i, 0) = (float)x1;
+    D2.at<float>(i, 1) = (float)y1;
+    D2.at<float>(i, 2) = (float)1;
+  }
+
+  std::cout << D1 << std::endl;
+  std::cout << D2 << std::endl;
+
+  cv::Mat S1 = D1.t() * D1;
+  std::cout << "S1" << S1 << std::endl;
+  cv::Mat S2 = D1.t() * D2;
+  std::cout << "S2" <<S2 << std::endl;
+  cv::Mat S3 = D2.t() * D2;
+  std::cout << "S3" << S3 << std::endl;
+
+  cv::Mat T = -S3.inv()*S2.t();
+  std::cout << "T" << T << std::endl;
+
+  cv::Mat M = S1 + S2*T;
+  std::cout << "M" << M << std::endl;
+  cv::Mat M2 = cv::Mat::zeros(cv::Size(3,3), M.type());
+  M2.at<float>(0,0) = M.at<float>(2,0)/2.0f;
+  M2.at<float>(0,1) = M.at<float>(2,1)/2.0f;
+  M2.at<float>(0,2) = M.at<float>(2,2)/2.0f;
+  M2.at<float>(1,0) = -M.at<float>(1,0);
+  M2.at<float>(1,1) = -M.at<float>(1,1);
+  M2.at<float>(1,2) = -M.at<float>(1,2);
+  M2.at<float>(2,0) = M.at<float>(0,0)/2.0f;
+  M2.at<float>(2,1) = M.at<float>(0,1)/2.0f;
+  M2.at<float>(2,2) = M.at<float>(0,2)/2.0f;
+  std::cout << "M2" << M2 << std::endl;
+
+  cv::Mat eigenvalues(1, 3, M2.type()), eigenvectors(3, 3, M2.type());
+  cv::eigenNonSymmetric(M2, eigenvalues, eigenvectors );
+
+  eigenvectors = -eigenvectors;
+  std::cout << "eigenvalues" << eigenvalues << std::endl;
+  std::cout << "eigenvectors" << eigenvectors << std::endl;
+
+  float check = 4*eigenvectors.at<float>(0,0) *eigenvectors.at<float>(0,2) - eigenvectors.at<float>(0,1)*eigenvectors.at<float>(0,1);
+  if(check < 0)
+    return false;
+
+  cv::Mat eigenvector_top = cv::Mat::zeros(cv::Size(1,3), eigenvectors.type());
+  cv::Mat T_eigenvector_top = cv::Mat::zeros(cv::Size(1,3), eigenvectors.type());
+  eigenvector_top.at<float>(0) = eigenvectors.at<float>(0,0);
+  eigenvector_top.at<float>(1) = eigenvectors.at<float>(0,1);
+  eigenvector_top.at<float>(2) = eigenvectors.at<float>(0,2);
+  T_eigenvector_top = T*eigenvector_top;
+  std::cout << "eigenvector_top\n" << eigenvector_top <<  std::endl;
+  std::cout << "T*eigenvector" << T_eigenvector_top <<  std::endl;
+
+
+  a = eigenvector_top.at<float>(0);
+  b = eigenvector_top.at<float>(1);
+  c = eigenvector_top.at<float>(2);
+  d = T_eigenvector_top.at<float>(0)-2*eigenvector_top.at<float>(0)*mx-eigenvector_top.at<float>(1)*my;
+  e = T_eigenvector_top.at<float>(1)-2*eigenvector_top.at<float>(2)*my-eigenvector_top.at<float>(1)*mx;
+  f  = T_eigenvector_top.at<float>(2) +
+      eigenvector_top.at<float>(0)*mx*mx +
+      eigenvector_top.at<float>(2)*my*my +
+      eigenvector_top.at<float>(1)*mx*my -
+      T_eigenvector_top.at<float>(0)*mx -
+      T_eigenvector_top.at<float>(1)*my;
+
+  return true;
 }
--- src/ed_circle.h
+++ src/ed_circle.h
@@ -20,6 +20,7 @@ class EDCircle {
   std::vector<std::vector<cv::Point>> find_least_squares_circle_fit(std::vector<std::vector<cv::Point> > edge_segments);
   std::vector<std::vector<cv::Point>> find_least_squares_ellipse_fit(std::vector<std::vector<cv::Point> > edge_segments);
   bool least_squares_circle_fit(std::vector<cv::Point> edge_segment, cv::Point2f &circle_center, float &radius);
+  bool least_squares_ellipse_fit(std::vector<cv::Point> edge_segment, float& a, float& b, float& c, float& d, float& e, float& f);
 };
 
 #endif //EDCIRCLE_SRC_ED_CIRCLE_H_
