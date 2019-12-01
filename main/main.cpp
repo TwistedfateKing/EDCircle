fd41f4aa96ef244d7ec0ed12e6d6aeee9c6bda79
twistedkateKing <esrobb3@gmail.com>
2019-12-02 01:09:52 +0900

    [#4] main display 부분 code 정리.
    .

--- main/main.cpp
+++ main/main.cpp
@@ -5,9 +5,42 @@
 #include "src/edge_drawing_parameter_free.h"
 #include "src/ed_circle.h"
 
+void DisplayEdgeSegmentResult( cv::Mat src_img, std::vector< std::vector<cv::Point> > edge_segment ) {
+
+  cv::Mat img_segments_color = cv::Mat::zeros(src_img.size(), CV_8UC3);
+  cv::Mat img_segment_white = cv::Mat::zeros(src_img.size(), CV_8UC3);
+
+  for(int i =0 ; i < edge_segment.size(); i++) {
+    cv::Vec3b ran_color = cv::Vec3b(rand()%255,rand()%255,rand()%255);
+    for (int j = 0; j < edge_segment[i].size(); j++) {
+      img_segments_color.at<cv::Vec3b>(edge_segment[i][j]) = ran_color;
+      img_segment_white.at<cv::Vec3b>(edge_segment[i][j]) = cv::Vec3b(255,255,255);
+    }
+  }
+  cv::hconcat(src_img, img_segments_color, img_segments_color);
+  cv::hconcat(img_segments_color, img_segment_white, img_segment_white);
+  cv::imshow("edge egment image", img_segment_white);
+}
+
+void DisplayCircleResult( cv::Mat src_img, std::vector<Circle> circles, std::vector<Ellipse> ellipses) {
+
+  cv::Mat ret_edcircle = cv::Mat::zeros(src_img.size(), CV_8UC3);
+  for (int i = 0 ; i < circles.size(); i++) {
+    std::cout <<circles[i].center << ", " << circles[i].radius << std::endl;
+    cv::circle(ret_edcircle, circles[i].center, circles[i].radius, cv::Scalar(60,255,60), 1, 8, 0 );
+  }
+
+  for(int i = 0 ; i < ellipses.size(); i++) {
+    std::cout <<ellipses[i].center << ", " << ellipses[i].radius_ls << "," <<  ellipses[i].angle << std::endl;
+    cv::ellipse(ret_edcircle, ellipses[i].center, ellipses[i].radius_ls, ellipses[i].angle, 0, 360, cv::Scalar(122,122,255));
+  }
+
+  cv::imshow("ret_circle", ret_edcircle);
+}
+
 int main(void)
 {
-  cv::Mat rena_img = cv::imread("main/circle_test.png");
+  cv::Mat rena_img = cv::imread("main/Lenna.png");
   cv::imshow("renna", rena_img);
 
 //  EdgeDrawing ed;
@@ -17,35 +50,12 @@ int main(void)
 
   EDCircle circle_detector;
   circle_detector.detect(edge_segment);
-
   std::vector<Circle> circle_ret = circle_detector.get_circle();
   std::vector<Ellipse> ellipse_ret = circle_detector.get_ellipse();
 
-  cv::Mat ret_img_segment = cv::Mat::zeros(rena_img.size(), CV_8UC3);
-  cv::Mat ret_img_edge = cv::Mat::zeros(rena_img.size(), CV_8UC3);
+  DisplayEdgeSegmentResult(rena_img, edge_segment);
+  DisplayCircleResult(rena_img, circle_ret, ellipse_ret );
 
-  for(int i =0 ; i < edge_segment.size(); i++) {
-    cv::Vec3b ran_color = cv::Vec3b(rand()%255,rand()%255,rand()%255);
-    for (int j = 0; j < edge_segment[i].size(); j++) {
-      ret_img_segment.at<cv::Vec3b>(edge_segment[i][j]) = ran_color;
-      ret_img_edge.at<cv::Vec3b>(edge_segment[i][j]) = cv::Vec3b(255,255,255);
-    }
-  }
-
-  cv::Mat ret_edcircle = cv::Mat::zeros(rena_img.size(), CV_8UC3);
-  for (int i = 0 ; i < circle_ret.size(); i++) {
-    std::cout <<circle_ret[i].center << ", " << circle_ret[i].radius << std::endl;
-    cv::circle(ret_edcircle, circle_ret[i].center, circle_ret[i].radius, cv::Scalar(60,255,60), 1, 8, 0 );
-  }
-
-  for(int i = 0 ; i < ellipse_ret.size(); i++) {
-    std::cout <<ellipse_ret[i].center << ", " << ellipse_ret[i].radius_ls << "," <<  ellipse_ret[i].angle << std::endl;
-    cv::ellipse(ret_edcircle, ellipse_ret[i].center, ellipse_ret[i].radius_ls, ellipse_ret[i].angle, 0, 360, cv::Scalar(122,122,255));
-  }
-
-  cv::imshow("ret_circle", ret_edcircle);
-  cv::imshow("ret_img_segment", ret_img_segment);
-  cv::imshow("ret_img_edge", ret_img_edge);
   cv::waitKey(0);
 
   return 1;
