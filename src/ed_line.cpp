f0ddee8e26faee773e14cdbe6a2f3b756a95adf7
twistedkateKing <esrobb3@gmail.com>
2019-12-01 22:35:59 +0900

    [#4] LineFit 생성, 및 코드 작성 중,.
    ..

--- src/ed_line.cpp
+++ src/ed_line.cpp
@@ -9,3 +9,52 @@ EDLine::EDLine() {
 EDLine::~EDLine() {
 
 }
+std::vector<Line> EDLine::detect(std::vector<std::vector<cv::Point> > edge_segment, cv::Size img_size ) {
+  // valid line length : -4log(N) / log (P);
+  line_valid_threshold_ = -4.0f * log((float)std::min(img_size.width, img_size.height)) / log(0.125f);
+
+  for (int i = 0; i < edge_segment.size(); i++) {
+    // fit line
+
+
+    // validation
+
+  }
+
+}
+
+void EDLine::LineFit(std::vector<cv::Point> pixelChain, int noPixels) {
+  double lineFitError = INFINITY; // current line fit error
+  LineEquation lineEquation; // y = ax + b OR x = ay + b
+  while (noPixels > MIN_LINE_LENGTH){
+    LeastSquaresLineFit(pixelChain, MIN_LINE_LENGTH, &lineEquation, &lineFitError);
+    if (lineFitError <= 1.0) break; // OK. An initial line segment detected
+    pixelChain++; // Skip the first pixel & try with the remaining pixels
+    noPixels–; // One less pixel
+  } // end-while
+
+  if (lineFitError > 1.0) return; // no initial line segment. Done.
+
+  // An initial line segment detected. Try to extend this line segment
+  int lineLen = MIN_LINE_LENGTH;
+  while (lineLen < noPixels){
+  double d = ComputePointDistance2Line(lineEquation, pixelChain[lineLen]);
+  if (d > 1.0) break;
+  lineLen++;
+  } //end-while
+  // End of the current line segment. Compute the final line equation & output it.
+  LeastSquaresLineFit(pixelChain, lineLen, &lineEquation);
+  Output ‘‘lineEquation’’
+  // Extract line segments from the remaining pixels
+  LineFit(pixelChain + lineLen, noPixels-lineLen);
+} //end-LineFit
+
+LineEquation::LineEquation() {
+
+}
+LineEquation::LineEquation(float A, float B) {
+
+}
+LineEquation::~LineEquation() {
+
+}
--- src/ed_line.h
+++ src/ed_line.h
@@ -4,12 +4,44 @@
 
 #ifndef EDCIRCLE_SRC_ED_LINE_H_
 #define EDCIRCLE_SRC_ED_LINE_H_
+
+#include <vector>
 #include <iostream>
+#include "opencv2/opencv.hpp"
+
+#define MIN_LINE_LENGTH 5
+
+struct Line {
+  cv::Point p1;
+  cv::Point p2;
+  float A, B; // y = Ax + B
+};
+
+class LineEquation {
+ public:
+  LineEquation();
+  LineEquation(float A, float B);
+  ~LineEquation();
+ protected:
+  Line line_;
+//  float A_, B_;
+//  cv::Point p1_;
+//  cv::Point p2_;
+};
 
 class EDLine {
  public:
   EDLine();
   ~EDLine();
+
+  std::vector<Line> detect( std::vector<std::vector<cv::Point> > edge_segment, cv::Size img_size );
+
+ protected:
+  void LineFit(std::vector<cv::Point> pixelChain, int noPixels);
+
+ protected:
+  std::vector<Line> ed_lines_;
+  float line_valid_threshold_ = 12;
 };
 
 #endif //EDCIRCLE_SRC_ED_LINE_H_
